c2811a4fa3070484665dc712fda1e129dadc7849
HatefulT <frilanser.xrenov@mail.ru>
2019-06-24 13:01:35 +0300

    createdCanvas

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-var setup = function() {
-  fullScreen();
+var w, h;
 
+var setup = function() {
+  w = window.innerWidth; h = window.innerHeight;
+  createCanvas(w, h);
 }
 
 var draw = function() {
   background(0);
-  
+
 }
