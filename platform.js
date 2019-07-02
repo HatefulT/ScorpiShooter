f232e4dcc492e5d0a76552edb780473bde9e64ee
HatefulT <frilanser.xrenov@mail.ru>
2019-07-03 00:58:26 +0300

    Reworked platforms

--- platform.js
+++ platform.js
@@ -1,20 +1,22 @@
-var PLATFORM_H = 20;
-
 var Platform = function(x, y, _w) {
   this.x = x;
   this.y = y;
   this.w = _w;
   this.texture = [];
+  const k = spritemap.platform.normal[0].h / PLATFORM_H;
   W = _w;
   this.texture.push(spritemap.platform.left);
-  W -= spritemap.platform.left.w;
-  W -= spritemap.platform.right.w;
+  W -= spritemap.platform.left.w*k;
+  W -= spritemap.platform.right.w*k;
 
   while(W > 0) {
     var r = floor(random(spritemap.platform.normal.length));
-    if(W - spritemap.platform.normal[r].w >= 0) {
+    if(W - spritemap.platform.normal[r].w*k >= 0) {
       this.texture.push(spritemap.platform.normal[r]);
-      W -= spritemap.platform.normal[r].w;
+      W -= spritemap.platform.normal[r].w*k;
+    } else {
+      this.texture.push(spritemap.platform.normal[6]);
+      W -= spritemap.platform.normal[6].w*k;
     }
   }
 
@@ -22,19 +24,18 @@ var Platform = function(x, y, _w) {
 }
 
 Platform.prototype.draw = function () {
-  // fill(20, 255, 40);
-  // noStroke();
-  // rect(this.x, this.y, this.w, PLATFORM_H);
+  const k = this.texture[0].h / PLATFORM_H;
   var _x = this.x;
   for(var i=0; i<this.texture.length; i++) {
     push();
     translate(_x, this.y);
-    drawSprite(this.texture[i]);
+    drawSprite(this.texture[i], k*this.texture[i].w, k*PLATFORM_H);
     pop();
-    _x += this.texture[i].w;
+    _x += k*this.texture[i].w;
   }
 };
 
 Platform.prototype.getCBox = function () {
-  return { x: this.x, y: this.y, x1: this.x+this.w, y1: this.y+PLATFORM_H };
+  const k = this.texture[0].h / PLATFORM_H;
+  return { x: this.x, y: this.y+2*k, x1: this.x+this.w, y1: this.y+PLATFORM_H-2*k };
 };
